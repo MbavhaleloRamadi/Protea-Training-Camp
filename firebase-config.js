0bf5db0a712b6d948374a99d70d57394b4d8e6bd
MbavhaleloRamadi <mbavharamadi@gmail.com>
2025-05-05 10:24:34 +0200

    Authentication Set Up

--- firebase-config.js
+++ firebase-config.js
@@ -5,14 +5,22 @@ exports.db = exports.auth = void 0;
 const app_1 = require("firebase/app");
 const auth_1 = require("firebase/auth");
 const firestore_1 = require("firebase/firestore");
+const analytics_1 = require("firebase/analytics");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_BUCKET",
-    messagingSenderId: "YOUR_MSG_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCLFOHGb5xaMSUtE_vgVO0aaY6MfLySeTs",
+    authDomain: "protea-training-camp.firebaseapp.com",
+    projectId: "protea-training-camp",
+    storageBucket: "protea-training-camp.firebasestorage.app",
+    messagingSenderId: "649833361697",
+    appId: "1:649833361697:web:5c402a67872ca10fe30e60",
+    measurementId: "G-K1HKHPG6HG"
 };
+// Initialize Firebase
 const app = (0, app_1.initializeApp)(firebaseConfig);
+const analytics = (0, analytics_1.getAnalytics)(app);
 exports.auth = (0, auth_1.getAuth)(app);
 exports.db = (0, firestore_1.getFirestore)(app);
